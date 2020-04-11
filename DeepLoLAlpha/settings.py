b73026f41fb0d81652dd0045b0b275e60588f9f3
Marko <markotopitsch@gmail.com>
2020-04-11 19:13:17 -0400

    Added dotenv

--- DeepLoLAlpha/settings.py
+++ DeepLoLAlpha/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RG_API_KEY = os.getenv("key")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
