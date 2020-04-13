025bfc8e7cd1d7694ac6f8146332c44871e8aae8
Marko <markotopitsch@gmail.com>
2020-04-12 20:50:10 -0400

    Added blank Summoner test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+
+from .models import Summoner
 
 # Create your tests here.
+
+class SummonerModelTests(TestCase):
+
+    # TODO: Should I be writing tests to verify the database constraints?
+    pass
